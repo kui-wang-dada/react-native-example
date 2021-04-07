91e849d67a09919cffb55f90fa4a5a42183d1c28
kui wang <kuiwang@wholeren.com>
2021-04-07 11:37:34 +0800

    版本修改

--- src/store/reducers/common.js
+++ src/store/reducers/common.js
@@ -3,7 +3,7 @@ import * as common from '../actions/common';
 const INITIAL_STATE = {
   barHeight: 0,
   theme: 'light',
-  version: '3.2.0',
+  version: '3.2.1',
   hasWechat: false,
   showErp: 'out',
 };
